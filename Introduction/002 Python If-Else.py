04ad7d304194a8572728df6e991954df9e3fd7b7
sohayelmahmud <sohayelmahmud612@gmail.com>
2025-11-06 22:27:22 +0600

    init

---
+++ Introduction/002 Python If-Else.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    mod = n % 2
+
+    if mod == 1:
+        print('Weird')
+
+    elif 2 <= n <= 5:
+        print('Not Weird')
+
+    elif 6 <= n <= 20:
+        print('Weird')
+
+    elif n > 20:
+        print('Not Weird')
